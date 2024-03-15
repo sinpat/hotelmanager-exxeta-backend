eba896819a790f1bd3067e8c23ead45d97d5f1b6
Patrick Singer <sinpat224@hotmail.com>
2024-03-15 11:27:14 +0100

    init spring boot

---
+++ src/main/kotlin/com/exxeta/hotelmanager/HotelmanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.exxeta.hotelmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotelmanagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/exxeta/hotelmanager/HotelmanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exxeta.hotelmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelmanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
